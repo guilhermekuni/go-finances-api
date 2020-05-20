5851f6732b305ba9db097ea469a75ec9513f175b
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2020-05-20 18:42:41 -0300

    implements delete transaction method

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,26 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transactionExists = transactionsRepository.find({
+      where: { id },
+    });
+
+    if (!transactionExists) {
+      throw new AppError('The transaction does not exists.');
+    }
+
+    await transactionsRepository.delete(id);
   }
 }
 

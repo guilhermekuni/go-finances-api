f84a2bbcae6bfcd8979d2d75b9dba32b813b90c0
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2020-05-20 18:19:02 -0300

    implements GET and POST route methods

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,4 +1,4 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, getRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
 
@@ -11,7 +11,33 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactionRepository = getRepository(Transaction);
+
+    const transactions = await transactionRepository.find();
+
+    const incomeTransactions = transactions.filter(
+      item => item.type === 'income',
+    );
+
+    const outcomeTransactions = transactions.filter(
+      item => item.type === 'outcome',
+    );
+
+    const income = incomeTransactions.reduce(
+      (total, transaction) => +total + +transaction.value,
+      0,
+    );
+
+    const outcome = outcomeTransactions.reduce(
+      (total, transaction) => +total + +transaction.value,
+      0,
+    );
+
+    return {
+      income,
+      outcome,
+      total: income - outcome,
+    };
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,55 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+
+    const databaseCategory = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    let category_id;
+
+    if (!databaseCategory) {
+      const newCategory = await categoriesRepository.create({
+        title: category,
+      });
+
+      const { id } = await categoriesRepository.save(newCategory);
+
+      category_id = id;
+    } else {
+      category_id = databaseCategory.id;
+    }
+
+    const transaction = await transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
